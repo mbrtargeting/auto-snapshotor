1738d3121c5fe2bfc3b7a5feed847ce0e38a44a8
Tony Tian <tony.tian@adscale.co.nz>
2018-10-07 18:29:45 +1300

    Turn off warning log in 3rd party lib

--- src/main/kotlin/org/adscale/autosnapshotor/Main.kt
+++ src/main/kotlin/org/adscale/autosnapshotor/Main.kt
@@ -3,11 +3,18 @@ package org.adscale.autosnapshotor
 import org.adscale.autosnapshotor.commandline.AutoSnapshotorCommands
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.slf4j.bridge.SLF4JBridgeHandler
 import picocli.CommandLine
+import java.util.logging.LogManager
 
 object Main {
 
     private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
+    init {
+        LogManager.getLogManager().reset()
+        SLF4JBridgeHandler.install()
+    }
 
     @JvmStatic
     fun main(args: Array<String>) {
